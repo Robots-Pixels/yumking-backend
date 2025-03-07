fa06c694ae31f39b653e4d255836e6b2f0374d78
Robots-Pixels <Robots-Pixels>
2025-03-07 17:31:37 +0100

    Making the backend deployable

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import bookingRouter from "./routes/booking.route.js";
+import cors from "cors";
+
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({origin: "*"}));
 app.use("/api/auth/", authRouter);
 app.use("/api/booking/", bookingRouter);
 
